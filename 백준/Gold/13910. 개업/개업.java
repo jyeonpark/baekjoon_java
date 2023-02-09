436c04f93f53409fcd081b741259ba2dd300d15c
jyeon <65281502+jyeonpark@users.noreply.github.com>
2023-02-09 17:43:30 +0900

    [Gold V] Title: 개업, Time: 224 ms, Memory: 18656 KB -BaekjoonHub

--- 백준/Gold/13910. 개업/개업.java
+++ 백준/Gold/13910. 개업/개업.java
@@ -9,42 +9,37 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static int n;
+	static final int MAX = 10001;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken()); // 짜장면의 수
-		int[] woks = new int[10001];
-		int[] dp = new int[n+1]; // dp[i] => i개의 짜장면을 만들기 위해 드는 최소 요리 횟수
-		Arrays.fill(dp, Integer.MAX_VALUE);
+		int[] woks = new int[MAX];
+		int[] dp = new int[n + 1]; // dp[i] => i개의 짜장면을 만들기 위해 드는 최소 요리 횟수
+		Arrays.fill(dp, MAX);
 		dp[0] = 0;
 		woks[0] = 1;
-		
+
 		int m = Integer.parseInt(st.nextToken()); // 웍의 수
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<m; i++) {
+		for (int i = 0; i < m; i++) {
 			woks[Integer.parseInt(st.nextToken())]++; // 웍 개수 증가
 		}
-		//System.out.println(woks[1] + " " + woks[2] + " " + woks[3] + " " + woks[4] + " " + woks[5]);
-		
-		
-		for(int i=1; i<=n; i++) {
-			for(int j=0; j<i; j++) {
-				if ((j==i-j && woks[j] >= 2)) { // 크기가 같은 웍 두개를 사용할 수 있는 경우
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j <= i/2; j++) {
+				if ((j == i - j && woks[j] >= 2)) { // 크기가 같은 웍 두개를 사용할 수 있는 경우
 					dp[i] = 1;
-					//System.out.println("i : " + i + "| 	" + "j : " + j + "| 	" + woks[j] + " " + woks[i-j]);
-				}
-				else if (j!=i-j && woks[j] > 0 && woks[i-j]>0) { // 크기가 다른 웍 두개를 사용할 수 있는 경우
+				} else if (j != i - j && woks[j] > 0 && woks[i - j] > 0) { // 크기가 다른 웍 두개를 사용할 수 있는 경우
 					dp[i] = 1;
-					//System.out.println("i : " + i + "| 	" + "j : " + j + "| 	" + woks[j] + " " + woks[i-j]);
-				}
-				else if (dp[j] != Integer.MAX_VALUE && dp[i-j]  != Integer.MAX_VALUE ) {
-					dp[i] = Math.min(dp[i], dp[j]+dp[i-j]);
+				} else if (dp[j] != MAX && dp[i - j] != MAX) {
+					dp[i] = Math.min(dp[i], dp[j] + dp[i - j]);
 				}
 			}
-			//System.out.printf("dp[%d] : %d\n",i,dp[i]);
+			//System.out.printf("dp[%d] : %d\n" ,i,dp[i]);
 		}
-		dp[n] = (dp[n] == Integer.MAX_VALUE ? -1 : dp[n]);
+		dp[n] = (dp[n] >= MAX ? -1 : dp[n]);
 		System.out.println(dp[n]);
 	}
 }
