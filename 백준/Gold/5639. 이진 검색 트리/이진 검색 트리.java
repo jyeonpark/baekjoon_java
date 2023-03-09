db10aa4d75aa870b4df180bfa68d1804839f48c1
jyeon <65281502+jyeonpark@users.noreply.github.com>
2023-03-09 22:35:33 +0900

    [Gold V] Title: 이진 검색 트리, Time: 344 ms, Memory: 19300 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -8,63 +8,45 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Node {
-		int data;
-		Node left;
-		Node right;
-
-		public Node(int data, Node left, Node right) {
-			this.data = data;
-			this.left = left;
-			this.right = right;
-		}
-
-		void addNode(int num) {
-			if (this.data > num) {
-				if (this.left == null) {
-					this.left = new Node(num, null, null);
-				}else
-					this.left.addNode(num);
-			} else {
-				if (this.right == null) {
-					this.right = new Node(num,null, null);
-				}else
-					this.right.addNode(num);
-			}
-		}
-
-	}
-
 	static StringBuilder sb;
 	static StringTokenizer st;
-	static Node root;
+	static ArrayList<Integer> nodes = new ArrayList<>();
+	static int size;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		sb = new StringBuilder();
-
-		root = new Node(Integer.parseInt(br.readLine()), null, null);
-		String line;
-		while ((line = br.readLine()) != null) {
-			int num = Integer.parseInt(line);
-			root.addNode(num);
+		String s;
+		while ((s = br.readLine()) != null) {
+			nodes.add(Integer.parseInt(s));
+			size++;
 		}
 		
-		postOrder(root);
+		postOrder(0, size-1);
+
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 	}
-	
-	static void postOrder(Node node) {
-		if (node.left != null) {
-			postOrder(node.left);
-		} 
-		if (node.right!=null) {
-			postOrder(node.right);
-		} 
-		sb.append(node.data).append("\n");
+
+	static void postOrder(int startNodeIdx, int endNodeIdx) {
+		if (startNodeIdx > endNodeIdx) return;
+		
+		int cur = nodes.get(startNodeIdx);
+		int startRightIdx = endNodeIdx+1;
+		
+		for(int i=startNodeIdx+1; i<=endNodeIdx; i++) {
+			if (nodes.get(i) > cur) {
+				startRightIdx = i;
+				break;
+			}
+		}
+		
+		postOrder(startNodeIdx+1, startRightIdx-1);
+		postOrder(startRightIdx, endNodeIdx);
+		
+		sb.append(cur).append("\n");
 	}
 
 }
