13d3806fedd70d36776d06ebc1f432cfdf9194eb
jyeon <65281502+jyeonpark@users.noreply.github.com>
2023-04-05 15:21:59 +0900

    [Gold IV] Title: 탈출, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,102 +1,101 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int r, c, time, hedgehog_x, hedgehog_y, cave_x, cave_y;
-	static char[][] map;
-	static Queue<Point> water;
-	static int[] dx = { 1, 0, -1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
+
+	static int r, c;
+	static int result = Integer.MAX_VALUE;
+	static char[][] arr;
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		r = Integer.parseInt(st.nextToken());
 		c = Integer.parseInt(st.nextToken());
-		map = new char[r][c];
-		water = new ArrayDeque<>();
+
+		arr = new char[r][c];
+
 		for (int i = 0; i < r; i++) {
-			map[i] = br.readLine().toCharArray();
+			String s = br.readLine();
 			for (int j = 0; j < c; j++) {
-				if (map[i][j] == 'D') {
-					cave_x = i;
-					cave_y = j;
-				} else if (map[i][j] == 'S') {
-					hedgehog_x = i;
-					hedgehog_y = j;
-					map[i][j] = '.'; // 고슴도치는 이동하므로 좌표 저장후 .으로 표시한다
-				} else if (map[i][j] == '*')
-					water.add(new Point(i, j));
+				arr[i][j] = s.charAt(j);
 			}
 		}
 
-		int time = bfs();
-		System.out.println(time == -1 ? "KAKTUS" : time);
+		bfs();
 
+		if (result == Integer.MAX_VALUE)
+			System.out.println("KAKTUS");
+		else
+			System.out.println(result);
 	}
 
-	static int bfs() {
-		Queue<Move> q = new ArrayDeque<>();
-		q.add(new Move(hedgehog_x, hedgehog_y, 0));
-		boolean[][] visited = new boolean[r][c]; // 고슴도치가 방문한 좌표
-		visited[hedgehog_x][hedgehog_y] = true;
-		while (!q.isEmpty()) {
+	static void bfs() {
+		Queue<int[]> s = new LinkedList<>();
+		Queue<int[]> w = new LinkedList<>();
+		boolean[][] visited = new boolean[r][c];
 
-			// 물이 확장한다.
-			int water_size = water.size();
-			for (int i = 0; i < water_size; i++) { // 1분에 확장될 수 있는 물
-				Point w = water.poll();
-				for (int d = 0; d < 4; d++) {
-					int nx = w.x + dx[d];
-					int ny = w.y + dy[d];
-					if (isRange(nx, ny) && map[nx][ny] == '.') { // 돌,소굴이 아니라면 확장 가능
-						map[nx][ny] = '*'; // 확장된 물 표시
-						water.add(new Point(nx, ny));
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				if (arr[i][j] == 'S') {
+					s.add(new int[] { i, j, 0 });
+					arr[i][j] = '.';
+					visited[i][j] = true;
+				}
+
+				if (arr[i][j] == '*') {
+					w.add(new int[] { i, j });
+				}
+			}
+		}
+
+		while (!s.isEmpty()) {
+			int water_size = w.size();
+			for (int k = 0; k < water_size; k++) {
+				int[] W = w.poll();
+				for (int i = 0; i < 4; i++) {
+					int wx = dx[i] + W[0];
+					int wy = dy[i] + W[1];
+
+					if (wx >= 0 && wx < r && wy >= 0 && wy < c) {
+						if (arr[wx][wy] == '.') {
+							arr[wx][wy] = '*';
+							w.add(new int[] { wx, wy });
+						}
 					}
 				}
 			}
 
-			// 비버가 움직인다.
-			int move_size = q.size();
-			for (int i = 0; i < move_size; i++) {
-				Move now = q.poll();
-				for (int d = 0; d < 4; d++) {
-					int nx = now.x + dx[d];
-					int ny = now.y + dy[d];
-					if (isRange(nx, ny)) {
-						if (nx == cave_x && ny == cave_y)
-							return now.time + 1;
-						if (map[nx][ny] == '.' && !visited[nx][ny]) {
-							q.add(new Move(nx, ny, now.time + 1));
-							visited[nx][ny] = true;
+			int move_size = s.size();
+			for (int k = 0; k < move_size; k++) {
+				int[] S = s.poll();
+				for (int i = 0; i < 4; i++) {
+					int sx = dx[i] + S[0];
+					int sy = dy[i] + S[1];
+					int cnt = S[2];
+
+					if (sx >= 0 && sx < r && sy >= 0 && sy < c && !visited[sx][sy]) {
+						if (arr[sx][sy] == '.') {
+							s.add(new int[] { sx, sy, cnt + 1 });
+							visited[sx][sy] = true;
+						}
+
+						if (arr[sx][sy] == 'D') {
+							result = cnt + 1;
+							return;
 						}
 					}
 				}
 			}
 		}
-		return -1;
+
 	}
 
-	static boolean isRange(int x, int y) {
-		return x >= 0 && x < r && y >= 0 && y < c;
-	}
-
-	static class Move {
-		int x;
-		int y;
-		int time;
-
-		public Move(int x, int y, int time) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.time = time;
-		}
-	}
 }
