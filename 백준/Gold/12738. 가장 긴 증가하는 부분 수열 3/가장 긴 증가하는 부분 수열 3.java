a87c2c0cd1925104c9767d2cce3ad010d578175d
jyeon <65281502+jyeonpark@users.noreply.github.com>
2023-04-07 13:34:48 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 568 ms, Memory: 121492 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] s = new int[N];
+		int[] LIS = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			s[i] = Integer.parseInt(st.nextToken());
+		}
+ 
+		LIS[0] = s[0];
+		int len = 1;
+		for (int i = 1; i < N; i++) {
+			int key = s[i];
+			// LIS의 마지막 값보다 클 경우 추가
+			if (LIS[len - 1] < key) {
+				len++;
+				LIS[len - 1] = key;
+			} 
+			else {
+				// 이분탐색으로 삽입할 장소 찾기 (key보다 큰 가장 작은 값 찾아서 대치하기)
+				int left = 0;
+				int right = len;
+				while (left < right) {
+					int mid = (left + right) / 2;
+					if(LIS[mid] < key) left = mid + 1;
+					else right = mid;
+				}
+				LIS[left] = key; // 대치
+			}
+		}
+		System.out.println(len);
+	}
+}
