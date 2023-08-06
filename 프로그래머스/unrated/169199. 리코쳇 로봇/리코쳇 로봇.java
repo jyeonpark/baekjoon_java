eae4e360041b5a169992a35ed134bf4d04226cdd
jyeon <65281502+jyeonpark@users.noreply.github.com>
2023-08-06 15:24:07 +0900

    [unrated] Title: 리코쳇 로봇, Time: 1.14 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -2,23 +2,25 @@ import java.util.*;
 
 class Solution {
     static int n,m,startX, startY,endX, endY;
+    static char[][] map;
     public int solution(String[] board) {
-        int answer = 0;
         n = board.length;
         m = board[0].length();
+        map = new char[n][m];
         for(int i=0; i<n; i++){
+            map[i] = board[i].toCharArray();
             for(int j=0; j<m; j++){
-                if (board[i].charAt(j) == 'R'){
+                if (map[i][j] == 'R'){
                     startX = i;
                     startY = j;
                     break;
                 } 
             }
         }
-        return bfs(board);
+        return bfs();
     }
     
-    static int bfs(String[] board){
+    static int bfs(){
         PriorityQueue<State> q = new PriorityQueue<>();
         int[] dx = {1,0,-1,0};
         int[] dy = {0,1,0,-1};
@@ -27,13 +29,13 @@ class Solution {
         q.add(new State(startX, startY, 0));
         while(!q.isEmpty()){
             State now = q.poll();
-            if (board[now.x].charAt(now.y) == 'G')  return now.cnt;
+            if (map[now.x][now.y] == 'G')  return now.cnt;
             for(int d = 0; d<4; d++){
                 int nx = now.x;
                 int ny = now.y;
                 while(true){
                     // 벽이거나, 장애물이라면
-                    if (!isRanged(nx+dx[d],ny+dy[d]) || board[nx+dx[d]].charAt(ny+dy[d]) == 'D'){
+                    if (!isRanged(nx+dx[d],ny+dy[d]) || map[nx+dx[d]][ny+dy[d]] == 'D'){
                         if (!visited[nx][ny]){ // 마지막 nx,ny를 방문하지 않았다면
                             visited[nx][ny] = true;
                             q.add(new State(nx, ny, now.cnt+1));
